d5b8726ac3fd7ac0348cdcda63c3711106579ed7
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-09-09 16:37:55 +0200

    Added fullscreen files, updated README, and more.

--- README.md
+++ README.md
@@ -3,5 +3,22 @@
 This is an introduction to JavaScript aimed at first year (and later) CS students.
 It will be the boilerplate for the 2nd lecture I hold for Code@LTH which will be held on the 10th september 2015.
 
-There will also be a jsfiddle, because everyone might not be familiar with Git and it's magic and jsfiddle is simply easier (even though they could just get a zip archive).
+There will also be a simpler jsfiddle, because everyone might not be familiar with Git and it's magic and jsfiddle is simply easier (even though they could just get a zip archive).
 
+Try making a game, some cool geometry or whatever brilliant idea your imagination comes up with!
+
+Here are a few learning resources:
+ - [HTML5 Canvas Tutorials](http://www.html5canvastutorials.com/)
+ - [A reference of all methods available on the context/'canvas' variable](http://www.w3schools.com/tags/ref_canvas.asp)
+ - [Phaser, a game development framework for canvas](http://phaser.io/)
+
+And here is a list of cool things you can try to make:
+ - [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+ - [Pythagoras Tree](https://en.wikipedia.org/wiki/Pythagoras_tree_(fractal))
+
+And here are a few things already made:
+ - [Badass animation](https://jsfiddle.net/gg19b2do/4/)
+ - [A Game of Life implementation using canvas by me](http://erik.bjareholt.com/GameOfLife/)
+
+Licensed under the [MIT License](http://opensource.org/licenses/MIT)
+(tl;dr You can do anything with it, but must give credit if it's a derivative work)
---
+++ fullscreen.js
@@ -0,0 +1,55 @@
+// Welcome to the fullscreen.js file!
+// Here you will discover a few new features, such as fullscreen canvas and a render-loop.
+// This file is used by the fullscreen.html file, and will not be loaded in the index.html file.
+// However, the fullscreen.html file ALSO loads script.js, so if you want to change something that
+// we did there (such as mouse and keyboard input) you'll have to do it in there.
+
+$(document).ready(function() {
+    // This variable stores the timestamp of the last drawed frame, used when calculating frames per second.
+    var lastTimestamp = new Date().getTime();
+
+    function drawFPSCounter(timestamp) {
+        // Calculates the number of frames per second
+        var fps = Math.round(10000 / (timestamp - lastTimestamp)) / 10;
+
+        // Here we can set the font we want to use when drawing text
+        canvas.font = "20px Arial";
+        canvas.fillText("FPS: " + fps, 10, 25);
+
+        // Store the timestamp from this frame to use when calculating the next frame.
+        lastTimestamp = timestamp;
+    }
+
+    function renderFrame(timestamp) {
+        // Write your code in here!
+        // It will run every time a new frame is drawn
+        // Just remember to keep "clear()", otherwise your frames will
+        // draw upon each other, creating a beautiful (or not) mess.
+        clear();
+
+        for(var i=0; i<20; i++) {
+            drawRectangle(canvas.width/2 - 200 + i*10, canvas.height/2 - 200 + i*10, 200, 200);
+        }
+
+        drawFPSCounter(timestamp);
+
+        // Tells the browser that the frame has been drawn and that we're ready to draw the next one.
+        window.requestAnimationFrame(renderFrame);
+    };
+
+    // This makes sure that the canvas keeps the size of the window if the window size changes.
+    $(window).resize(function() {
+        canvasElement.width = window.innerWidth;
+        canvasElement.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    });
+
+    function onLoad() {
+        // If you wish to do something once and only once when the program starts, do it here!
+        $(window).resize();
+        renderFrame();
+    };
+
+    onLoad();
+});
